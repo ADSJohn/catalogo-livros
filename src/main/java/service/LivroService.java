2a3c94b7ed84e446fca5a2eeea5cfd61bf84d4fa
ADSJohn <progjohn@outlook.com>
2024-12-27 20:06:57 -0300

    Adicao do LivroService

--- src/main/java/service/LivroService.java
+++ src/main/java/service/LivroService.java
@@ -1,4 +1,29 @@
 package service;
 
+import conversor.LivroConversor;
+import dto.LivroDto;
+import jakarta.inject.Inject;
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebService;
+import jakarta.transaction.Transactional;
+import repository.LivroRepository;
+
+import java.util.List;
+
+@WebService
 public class LivroService {
+
+	@Inject
+	LivroConversor livroConversor;
+
+	@Inject
+	LivroRepository livroRepository;
+
+	@Transactional
+	@WebMethod
+	public LivroDto create(LivroDto livroDto) {
+		var livro = livroConversor.dtoToEntity(livroDto);
+		livroRepository.persist(livro);
+		return livroConversor.entityToDto(livro);
+	}
 }
