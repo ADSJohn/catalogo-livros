a3d8aff06066c856ba378a845931c70c4d8920d2
ADSJohn <progjohn@outlook.com>
2024-12-27 19:35:06 -0300

    Adicao do DTO de Livro

---
+++ src/main/java/dto/LivroDto.java
@@ -0,0 +1,56 @@
+package dto;
+
+import java.util.List;
+
+public class LivroDto {
+
+	private String id;
+
+	private String nome;
+
+	private List<String> autores;
+
+	private String editora;
+
+	private Integer anoDePublicacao;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<String> getAutores() {
+		return autores;
+	}
+
+	public void setAutores(List<String> autores) {
+		this.autores = autores;
+	}
+
+	public String getEditora() {
+		return editora;
+	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
+	public Integer getAnoDePublicacao() {
+		return anoDePublicacao;
+	}
+
+	public void setAnoDePublicacao(Integer anoDePublicacao) {
+		this.anoDePublicacao = anoDePublicacao;
+	}
+}
